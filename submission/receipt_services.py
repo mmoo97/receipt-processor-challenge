5cad8a48d10ffb650cc3c98deb63bfa975a6fb5c
Mitchell Moore <mmoo97@uab.edu>
2023-06-16 16:02:54 -0500

    Add basic functionality for storing receipts.'

--- submission/receipt_services.py
+++ submission/receipt_services.py
@@ -1,13 +1,40 @@
 import uuid
 import json
-import receipt_schema as rschema
+from openapi_spec_validator import validate_spec
+from openapi_spec_validator.readers import read_from_filename
+from openapi_schema_validator import validate
+
+spec_dict, spec_url = read_from_filename('../api.yml')
+
+validate_spec(spec_dict)
+
+file = open('schema.json')
+
+schema = json.load(file)
+
+file.close()
+
+in_mem_db = {}
 
 def process_receipt(request):
 
-    return { "id": str(uuid.uuid4()) }
+    # print(request.json)
+
+    # if validate(request.json, schema):
+    #     print(True)
+    # else:
+    #     print(False)
+
+    receipt = request.json
+
+    id = str(uuid.uuid4())
+
+    in_mem_db[id] = receipt
+
+    return { "id": id }
 
 def get_points(id):
-    return "<p>Hello, points {}!</p>".format(id)
+    return { "points": in_mem_db[id] }
 
 def __score_points(receipt):
     pass
