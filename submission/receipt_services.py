c53d646cbaad48113a6d66c9a20c1263e21e57f3
Mitchell Moore <mmoo97@uab.edu>
2023-06-16 20:53:06 -0500

    Finish scoring functions.

--- submission/receipt_services.py
+++ submission/receipt_services.py
@@ -3,6 +3,8 @@ import json
 from openapi_spec_validator import validate_spec
 from openapi_spec_validator.readers import read_from_filename
 from openapi_schema_validator import validate
+import math
+from datetime import datetime
 
 spec_dict, spec_url = read_from_filename('../api.yml')
 
@@ -40,9 +42,14 @@ def get_points(id):
 
 def __score_points(receipt):
     total = 0
+
     total +=  __score_retailer_points(receipt)
     total +=  __score_dollar_amount(receipt)
     total += __score_item_counts(receipt)
+    total += __score_item_descriptions(receipt)
+    total += __score_date(receipt)
+    total += __score_time(receipt)
+
     return total
 
 def __score_retailer_points(receipt):
@@ -60,8 +67,49 @@ def __score_item_counts(receipt):
     score = 0
     score =+ len(receipt['items']) // 2 * 5
     return score
+
+def __score_item_descriptions(receipt):
+    score = 0.0
+    for item in receipt['items']:
+        if len(item['shortDescription']) % 3 == 0:
+            item_score = float(item['price']) * 0.2
+            score += math.ceil(item_score)
+
+    return int(score)
+
+
+def __score_date(receipt):
+    score = 0
+
+    day = datetime.strptime(receipt['purchaseDate'], '%Y-%m-%d').date()
     
-     
+    if __is_odd(day.day):
+        score += 6
+
+    return score
+
+def __score_time(receipt):
+    score = 0
+
+    time = datetime.strptime(receipt['purchaseTime'], '%H:%M')
+
+    if __is_in_time_range(time):
+        score += 10
+
+    return score
+
+def __is_odd(num):
+    if num % 2 != 0:
+        return True
+    return False
+
+def __is_in_time_range(time):
+
+    if 14 < time.hour < 16:
+        return True
+    
+    return False
+
 
 def validateJSON(jsonData):
     try:
